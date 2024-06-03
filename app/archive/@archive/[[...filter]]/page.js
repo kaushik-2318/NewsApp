de9044f6c4443b0dbc9794765467429fb90623ad
kaushik-2318 <kaushikverma321@gmail.com>
2024-06-03 11:10:42 +0530

    Catch-All Fallback Routes & Dealing With Multiple Path Segments

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,54 @@
+import NewsList from "@/components/news-list"
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+import Link from "next/link";
+
+function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectYear = filter?.[0];
+  const selectMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears()
+
+  if (selectYear && !selectMonth) {
+    news = getNewsForYear(selectYear)
+    links = getAvailableNewsMonths(selectYear);
+  }
+
+  if(selectYear && selectMonth){
+    news = getNewsForYearAndMonth(selectYear, selectMonth);
+    links =[];
+  }
+
+  let newsContent = <p>No news found for the selected period</p>
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />
+  }
+
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectYear ? `/archive/${selectYear}/${link}` : `/archive/${link}`
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}
+                  </Link>
+                </li>
+              )
+            }
+            )}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  )
+}
+
+export default FilteredNewsPage
